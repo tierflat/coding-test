0b91df59b3a64c2517229b220fd7ceb650d7e364
Fred Ricaforte <agilephp@gmail.com>
2024-12-20 12:38:10 +0800

    retrieve by key

--- app/Http/Controllers/ObjectController.php
+++ app/Http/Controllers/ObjectController.php
@@ -88,14 +88,34 @@ class ObjectController extends Controller
     
     }
 
-    public function show($id) {
-        $object = Objects::find($id);
-        if(!empty($object)) {
-            return response()->json($object);
-        } else {
+    public function show($key) {
+        try {
+            // check if timestamp is passed
+            $timestamp = request('timestamp');
+            if($timestamp && !is_numeric($timestamp)) {
+                return response()->json([
+                    'message' => 'Invalid timestamp'
+                ], Response::HTTP_BAD_REQUEST);
+            }
+
+            if($timestamp) {
+                $object = Objects::getByTimestamp($key, $timestamp);
+            } else {
+                $object = Objects::getLatestValue($key);
+            }
+            if(!$object) {
+                return response()->json([
+                    'message' => 'Object not found'
+                ], Response::HTTP_NOT_FOUND);
+            }
             return response()->json([
-                "message" => "Object not found!"
-            ], 404);
+                $object->value,
+            ], Response::HTTP_OK);
+        } catch(\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'error' => $e->getMessage()
+            ], 500);
         }
 
     }
